4ffc154295868c4306b1013e4f9839719ea90047
MC <chuyik@gmail.com>
2018-03-01 15:55:53 +0800

    added in UIConfig constant, wired up nib elements

---
+++ AppListing/Configurations/UIConfig.swift
@@ -0,0 +1,17 @@
+//
+//  UIConfig.swift
+//  AppListing
+//
+//  Created by MC on 1/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import Foundation
+
+enum UIConfig
+{
+    enum SearchBar
+    {
+        static let placeholder = "搜尋"
+    }
+}
--- AppListing/ViewControllers/MainViewController.swift
+++ AppListing/ViewControllers/MainViewController.swift
@@ -8,27 +8,33 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
+class MainViewController: UIViewController
+{
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var freeAppTableView: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        customiseSearchBar()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override var prefersStatusBarHidden: Bool
+    {
+        return true
     }
-    */
+}
 
+// MARK: - Search Bar
+extension MainViewController
+{
+    private func customiseSearchBar()
+    {
+        searchBar.placeholder = UIConfig.SearchBar.placeholder
+    }
 }
