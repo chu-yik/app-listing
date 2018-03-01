009b61a0f153d71cb91681d317f9ce4a490c968b
MC <chuyik@gmail.com>
2018-03-01 16:51:36 +0800

    added parsing key protocol and implementation for response JSON

---
+++ AppListing/Parsing/ITunesRssParsingKey.swift
@@ -0,0 +1,20 @@
+//
+//  ITunesRssParsingKey.swift
+//  AppListing
+//
+//  Created by MC on 1/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import Foundation
+
+struct ITunesRssParsingKey: ParsingKeyProtocol
+{
+    let dataStart: [String] = ["feed", "entry"]
+    let id: [String] = ["id", "attributes", "im:id"]
+    let name: [String] = ["im:name", "label"]
+    let category: [String] = ["category", "attributes", "label"]
+    let artist: [String] = ["im:artist", "label"]
+    let summary: [String] = ["summary", "label"]
+    let imageUrl: [String] = ["im:image", "label"]
+}
---
+++ AppListing/Parsing/ParsingKeyProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  ParsingKeyProtocol.swift
+//  AppListing
+//
+//  Created by MC on 1/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import Foundation
+
+protocol ParsingKeyProtocol
+{
+    var dataStart: [String] { get }
+    var id: [String] { get }
+    var name: [String] { get }
+    var category: [String] { get }
+    var artist: [String] { get }
+    var summary: [String] { get }
+    var imageUrl: [String] { get }
+}
