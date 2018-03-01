149eb993f474e1eb71695a3348a42e8c3ea1cbf0
MC <chuyik@gmail.com>
2018-03-01 16:41:45 +0800

    created MO for App

---
+++ AppListing/Models/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  AppListing
+//
+//  Created by MC on 1/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import Foundation
+
+struct App
+{
+    let id: String
+    let name: String
+    let category: String
+    let artist: String
+    let summary: String
+    let imageUrl: String
+}
