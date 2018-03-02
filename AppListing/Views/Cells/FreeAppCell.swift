4fa5cf2639fc90ca752fd567efc7d28475637e99
MC <chuyik@gmail.com>
2018-03-02 10:29:10 +0800

    partly implemented FreeAppCell, rating view update not implemented yet

--- AppListing/Views/Cells/FreeAppCell.swift
+++ AppListing/Views/Cells/FreeAppCell.swift
@@ -7,18 +7,58 @@
 //
 
 import UIKit
+import Cosmos
 
-class FreeAppCell: UITableViewCell {
-
-    override func awakeFromNib() {
+class FreeAppCell: UITableViewCell
+{
+    static let identifier = "FreeAppCell"
+    
+    @IBOutlet weak var appImageView: UIImageView!
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var ratingView: CosmosView!
+    
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    var index: Int? {
+        didSet {
+            if let index = index
+            {
+                indexLabel.text = "\(index)"
+                configureImageView()
+            }
+        }
+    }
+    
+    var app: App? {
+        didSet {
+            if let app = app
+            {
+                nameLabel.text = app.name
+                categoryLabel.text = app.category
+                updateImageFrom(app.imageUrl)
+                // TODO: ratings
+            }
+        }
+    }
+    
+    private func updateImageFrom(_ imageUrl: String)
+    {
+        if let url = URL(string: imageUrl)
+        {
+            appImageView.kf.indicatorType = .activity
+            appImageView.kf.setImage(with: url)
+        }
+    }
+    
+    private func configureImageView()
+    {
+        appImageView.contentMode = .scaleAspectFit
+        // TODO: round or circle clipping here
+        appImageView.clipsToBounds = true
+    }
 }
