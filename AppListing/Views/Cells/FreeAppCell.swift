5a8cc67a88e3e13ca571a90d03fa626b8d42fee8
MC <chuyik@gmail.com>
2018-03-02 10:08:12 +0800

    added stubs for FreeAppCell and started implementing it

---
+++ AppListing/Views/Cells/FreeAppCell.swift
@@ -0,0 +1,24 @@
+//
+//  FreeAppCell.swift
+//  AppListing
+//
+//  Created by MC on 2/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import UIKit
+
+class FreeAppCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
