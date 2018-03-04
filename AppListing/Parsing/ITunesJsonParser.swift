885fab2bfb818442866e24fb5abaeae360a780ec
MC <chuyik@gmail.com>
2018-03-04 09:50:11 +0800

    started refactoring parsing logic from data source, created protocol for json parser, and implementation stub for itunes json parser, some organizing of the files in Parsing/ directory

---
+++ AppListing/Parsing/ITunesJsonParser.swift
@@ -0,0 +1,9 @@
+//
+//  ITunesJsonParser.swift
+//  AppListing
+//
+//  Created by MC on 4/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import Foundation
---
+++ AppListing/Parsing/JsonParserProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  JsonParserProtocol.swift
+//  AppListing
+//
+//  Created by MC on 4/3/2018.
+//  Copyright © 2018 MC. All rights reserved.
+//
+
+import SwiftyJSON
+
+/// Defines protocol for a JSON parser being used by the AppDataSourceProtocol
+protocol JsonParserProtocol
+{
+    func parse(json: JSON) -> [App]
+    func parseDetail(json: JSON) -> [AppWithRating]
+}
